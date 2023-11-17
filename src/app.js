1bbbd1aa8d496b9f3851025ea27e3a96c5aaaaa8
angeljimenez91 <139405887+angeljimenez91@users.noreply.github.com>
2023-11-17 20:18:25 +0000

    primer

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+      for (var k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
 };
